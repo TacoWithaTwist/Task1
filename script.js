b6d0119ac6564082c31d6d2bc7e93d3516370cc9
TacoWithaTwist <houssein.msallem@gmail.com>
2024-12-17 11:12:27 +0100

    cool

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-const themes = [{}];
+const themes = [
+  { mainColor: ' #2b303a', accentColor: '#92dce5', secondaryColor: 'white' },
+  { mainColor: ' #F2FF49', accentColor: '#FF4242', secondaryColor: 'black' },
+  { mainColor: ' #502419', accentColor: '#7EA172', secondaryColor: 'white' },
+];
 function handleTheme() {
   const themeButton = document.getElementById('themeButton');
-  themeButton.addEventListener('click', () => {});
+  const root = document.documentElement;
+  let index = 0;
+  themeButton.addEventListener('click', () => {
+    if (index == themes.length - 1) {
+      index = -1;
+    }
+    index += 1;
+    currentColorPallette = themes[index];
+    root.style.setProperty('--main-color', `${currentColorPallette.mainColor}`);
+    root.style.setProperty(
+      '--accent-color',
+      `${currentColorPallette.accentColor}`
+    );
+    root.style.setProperty(
+      '--secondary-color',
+      `${currentColorPallette.secondaryColor}`
+    );
+    const currentColor = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue('--main-color');
+    console.log(currentColor);
+  });
 }
+handleTheme();
